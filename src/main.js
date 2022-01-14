e6e9febe16ac50be893e2e820f7461e90b4994c8
Duc Hai <haihamhb1@gmail.com>
2022-01-14 16:01:37 +0700

    lab1

--- src/main.js
+++ src/main.js
@@ -1,12 +1,51 @@
-const menuList = ["menu 1", "menu 2", "menu 3"];
+import Navigo from "navigo";
+import DetailNewsPage from "./pages/detailNews";
+import HomePage from "./pages/home";
 
-//b1: Selector
-//b2: vong lap(loop)
-//b3: render
-const menuElement = document.querySelector('#menu');
-  if(menuElement){
-    for(let i = 0; i < menuList.length; i++){
-      menuElement.innerHTML += `<li>${menuList[i]}</li>`;
-  }
-}
-// template string
+const router = new Navigo("/", { linksSelector: "a" });
+const print = (content) => {
+    document.getElementById("app").innerHTML = content;
+};
+
+router.on({
+    "/": () => {
+        print(HomePage.render());
+    },
+    "/about": () => {
+        print("About Page");
+    },
+    "/product": () => {
+        print("Product Page");
+    },
+    "/news/:id": (value) => {
+        console.log(value.data.id);
+        print(DetailNewsPage.render(value.data.id));
+    },
+});
+
+router.notFound(() => print("Not Found Page"));
+
+router.resolve();
+
+// class KhuanBanh {
+//     constructor(luongDuong, luongBot) {
+//         this.duong = luongDuong;
+//         this.bot = luongBot;
+//     }
+
+//     showInfo() {
+//         console.log(`
+//             Lượng đường: ${this.duong}
+//             Lượng Bột: ${this.bot}
+//         `);
+//     }
+// }
+
+// const banhDeo = new KhuanBanh(10, 20);
+// banhDeo.showInfo();
+// console.log(banhDeo.duong);
+
+// function sum(a, b) {
+//     return a + b;
+// }
+// sum(3, 4);
